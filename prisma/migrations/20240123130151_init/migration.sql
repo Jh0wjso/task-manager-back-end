faea480cef72786950966410ba7e4ce22f3bcd16
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-01-23 10:12:22 -0300

    Setup prisma

---
+++ prisma/migrations/20240123130151_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "isFavorite" BOOLEAN NOT NULL DEFAULT false
+);
