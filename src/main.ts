c91409bae82e40866e5a07361031bd23e4e2170d
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-01-23 10:17:32 -0300

    Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3030);
+
+  const config = new DocumentBuilder();
+  config.setTitle('API');
+  config.setDescription('API description');
+  config.setVersion('1.0');
+  config.addTag('API');
+  const document = SwaggerModule.createDocument(app, config.build());
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
