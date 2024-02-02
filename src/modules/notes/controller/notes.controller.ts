efd59f905fe8eab171f58dd908688134222f1e24
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-02-02 11:51:48 -0300

    :sparkles: feat: Update on notes controller

--- src/modules/notes/controller/notes.controller.ts
+++ src/modules/notes/controller/notes.controller.ts
@@ -2,7 +2,7 @@ import { Controller } from '@nestjs/common';
 import { NotesService } from '../services/notes.service';
 import { ApiTags } from '@nestjs/swagger';
 
-@Controller('notes')
+@Controller('Notes')
 @ApiTags('Notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
--- src/modules/notes/notes.module.ts
+++ src/modules/notes/notes.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NotesService } from './services/notes.service';
-import { NotesController } from './contoller/notes.controller';
+import { NotesController } from './controller/notes.controller';
 
 @Module({
   controllers: [NotesController],
