6f3c3719191ed2269d25c6499b610bb632e71281
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-02-07 14:20:56 -0300

    :sparkles: feat: Note modules update

--- src/modules/notes/notes.module.ts
+++ src/modules/notes/notes.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotesService } from './services/notes.service';
 import { NotesController } from './controller/notes.controller';
+import { NotesRepository } from './repositories/notes.repositories';
 
 @Module({
   controllers: [NotesController],
-  providers: [NotesService],
+  providers: [NotesService, NotesRepository],
+  exports: [NotesService, NotesRepository],
 })
 export class NotesModule {}
