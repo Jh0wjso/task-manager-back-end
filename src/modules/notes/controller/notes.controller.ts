300ba08614880f958c3b5223920ba4ac07204d48
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-02-08 13:22:04 -0300

    :sparkles: feat: Finish Note Controller

--- src/modules/notes/controller/notes.controller.ts
+++ src/modules/notes/controller/notes.controller.ts
@@ -1,9 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { NotesService } from '../services/notes.service';
 import { ApiTags } from '@nestjs/swagger';
+import { CreateNoteDto } from '../dtos/create-note.dto';
 
 @Controller('Notes')
 @ApiTags('Notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
+
+  @Post()
+  async createNote(@Body() createNoteDto: CreateNoteDto) {
+    return this.notesService.create(createNoteDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.notesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(id: string) {
+    return this.notesService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Body() updateNoteDto: CreateNoteDto, id: string) {
+    return this.notesService.update(id, updateNoteDto);
+  }
+
+  @Delete(':id')
+  async remove(id: string) {
+    return this.notesService.remove(id);
+  }
 }
