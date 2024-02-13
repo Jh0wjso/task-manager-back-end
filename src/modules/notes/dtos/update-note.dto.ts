7e17c6ac67716870c48b2fc405de753f8f7f2bfc
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-02-12 22:36:30 -0300

    :sparkles: feat: Notes DTO Update

--- src/modules/notes/dtos/update-note.dto.ts
+++ src/modules/notes/dtos/update-note.dto.ts
@@ -14,4 +14,25 @@ export class UpdateNoteDto {
     example: 'Note content',
   })
   content: string;
+
+  @ApiProperty({
+    required: false,
+    type: String,
+    example: '2021-10-10T10:00:00.000Z',
+  })
+  createdAt: string;
+
+  @ApiProperty({
+    required: false,
+    type: String,
+    example: '2021-10-10T10:00:00.000Z',
+  })
+  updatedAt: string;
+
+  @ApiProperty({
+    required: false,
+    type: Boolean,
+    example: true,
+  })
+  isFavorite: boolean;
 }
